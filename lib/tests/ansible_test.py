686bd6ed53157272d2eee40858b85ebf7df7e818
Ed Epstein <ed.epstein@invokelabs.com>
2013-09-27 11:32:27 -0700

    LABSPLAT-18 #comment Add dependency tests for vagrant, add roles from submodule, modify how testing is done using util functions.

--- lib/tests/ansible_test.py
+++ lib/tests/ansible_test.py
@@ -1,16 +1,6 @@
+import util
 
 def test():
 	"""Tests for the existence of the ansible program."""
-	print "...Checking if ansible is installed..."
-	try:
-		import ansible
-	except ImportError:
-		raise Exception('\tAnsible is missing. Install it.')
-	print "\tansible is installed, checking version..."
-	# test the version of ansible, we are aiming to require greater than or equal to 1.2.2
-	from distutils.version import StrictVersion
 	minimum_version = "1.2.2"
-	if StrictVersion(ansible.__version__) < StrictVersion(minimum_version):
-		raise Exception('\tansible is installed, but version is less than', minimum_version)
-	# all good.
-	print "\tansible version is greater than or equal to", minimum_version
+	util.module_version_check(minimum_version, "ansible")
--- wscript
+++ wscript
@@ -2,12 +2,15 @@
 # encoding: utf-8
 
 def check(ctx):
+	"""Check that all dependencies are installed and at least the minimum required version."""
 	print('Looking for dependencies...')
 	from lib.tests import boto_test
 	from lib.tests import ansible_test
+	from lib.tests import vagrant_test
 	try:
 		boto_test.test()
 		ansible_test.test()
+		vagrant_test.test()
 	except Exception as inst:
 			print inst.args
 
