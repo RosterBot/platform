b37b81803a293aa185d7757de67c8e2dff16acef
Ed Epstein <ed.epstein@invokelabs.com>
2013-10-29 23:32:04 -0700

    Slight tweak to minimum ansible version, and add sandbox and pycharm files to .gitignore.

--- lib/tests/ansible_test.py
+++ lib/tests/ansible_test.py
@@ -1,6 +1,6 @@
 import util
 
 def test():
-	"""Tests for the existence of the ansible program."""
-	minimum_version = "1.2.2"
-	util.module_version_check(minimum_version, "ansible")
+    """Tests for the existence of the ansible program."""
+    minimum_version = "1.3.0"
+    util.module_version_check(minimum_version, "ansible")
--- lib/tests/util.py
+++ lib/tests/util.py
@@ -1,21 +1,22 @@
 
 def compare_versions(minimum, current, program_name):
-	# test the version of the program, we are aiming to require greater than or equal to the minimum version
-	print "\t" + program_name + " is installed, checking version..."
-	from distutils.version import StrictVersion
-	if StrictVersion(current) != StrictVersion(minimum):
-		raise Exception("\t" + program_name + " is installed, but version is not " + minimum + ", we require version "+current)
-	# all good.
-	print "\tGood, " + current + " is equal to " + minimum
+    # test the version of the program, we are aiming to require greater than or equal to the minimum version
+    print "\t" + program_name + " is installed, checking version..."
+    from distutils.version import StrictVersion
+    if StrictVersion(current) < StrictVersion(minimum):
+        raise Exception("\t" + program_name + " is installed, but version is not " + minimum + ", we require version " +
+                        current)
+    # all good.
+    print "\tGood, " + current + " is greater than " + minimum
 
 def module_version_check(minimum_version, program_name, readable_name=None):
-	"""Tests for the existence of a particular version of a library."""
-	printable_name = program_name
-	if readable_name:
-		printable_name = readable_name
-	print "...Checking if " + printable_name + " library is installed..."
-	try:
-		module = __import__(program_name)
-	except ImportError:
-		raise Exception("\t" + printable_name + " is missing. Install it.")
-	compare_versions(minimum_version, module.__version__, printable_name)
+    """Tests for the existence of a particular version of a library."""
+    printable_name = program_name
+    if readable_name:
+        printable_name = readable_name
+    print "...Checking if " + printable_name + " library is installed..."
+    try:
+        module = __import__(program_name)
+    except ImportError:
+        raise Exception("\t" + printable_name + " is missing. Install it.")
+    compare_versions(minimum_version, module.__version__, printable_name)
