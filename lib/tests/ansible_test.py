a930acd09027bf525f116cdd02c77e42635bc8a3
Ed Epstein <ed.epstein@invokelabs.com>
2014-01-08 15:07:08 -0800

    LABSPLAT-18 Bump submodule and bump version of Ansible required in platform dependency tests.

--- lib/tests/ansible_test.py
+++ lib/tests/ansible_test.py
@@ -2,5 +2,5 @@ import util
 
 def test():
     """Tests for the existence of the ansible program."""
-    minimum_version = "1.3.0"
+    minimum_version = "1.4.0"
     util.module_version_check(minimum_version, "ansible")
